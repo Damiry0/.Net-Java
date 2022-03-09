93558e5f715f6b7f12a025a39123423ec3dcdbb0
Damian Ryś <55887851+Damiry0@users.noreply.github.com>
2022-03-09 01:35:11 +0100

    more refactoring

--- .Net/Lab1/Dynamic/Dynamic/Program.cs
+++ .Net/Lab1/Dynamic/Dynamic/Program.cs
@@ -4,9 +4,10 @@ Console.WriteLine("Input bag capacity:");
 var bagTarget = new Bag(int.Parse(s: Console.ReadLine()));
 Console.WriteLine("Input number of items:");
 var bagSource = new Bag(int.Parse(s: Console.ReadLine()));
-
 Console.WriteLine("Input seed:");
 bagSource.GenerateRandomBackpack(int.Parse(Console.ReadLine()));
+
+
 Console.WriteLine("Generated backpack:");
 bagSource.Display();
 Console.WriteLine("Sorted generated backpack:");
@@ -15,14 +16,3 @@ bagSource.Display();
 Console.WriteLine("Solved:");
 bagTarget.KnapSack(bagSource);
 bagTarget.Display();
-
-
-
-// Console.WriteLine("Vault:");
-// bag.Display();
-// Console.WriteLine("Backpack:");
-// var backpack = bag.KnapSack();
-// foreach (var item in backpack)
-// {
-//     Console.WriteLine("Weight:{0},Value:{1},Ratio:{2}",item.weight,item.value,item.Ratio);
-// }
