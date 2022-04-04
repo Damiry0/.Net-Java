27d9c9de35ddd16b880d47d4023488b12dbd7f6e
parapapere <parapapere@gmail.com>
2022-04-04 17:47:58 +0200

    Entity framework, base

---
+++ .Net/Lab2/FetchAPI/FetchAPI/Films.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Data.Entity;
+
+namespace FetchAPI
+{
+	public class Films : DbContext
+	{
+		public virtual DbSet<Movie> Movies { get; set; }	
+	}
+}
