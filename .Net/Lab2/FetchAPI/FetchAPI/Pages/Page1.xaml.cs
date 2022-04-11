a66397cd56492b8ca6f8946938630a4efa580df1
Damian Ryś <55887851+Damiry0@users.noreply.github.com>
2022-04-11 15:37:58 +0200

    Added sorting by field in MyList

--- .Net/Lab2/FetchAPI/FetchAPI/Pages/Page1.xaml.cs
+++ .Net/Lab2/FetchAPI/FetchAPI/Pages/Page1.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using HandyControl.Data;
+using Newtonsoft.Json;
 
 namespace FetchAPI.Pages
 {
@@ -20,9 +22,30 @@ namespace FetchAPI.Pages
     /// </summary>
     public partial class Page1 : Page
     {
+        public Movie movies { get; set; }
+
+        public Films films { get;}
+
         public Page1()
         {
             InitializeComponent();
+            films = new Films();
+            movies = new Movie();
+        }
+
+        private void ButtonShowDetailed_OnClick(object sender, RoutedEventArgs e)
+        {
+            gridFilms.ItemsSource = films.Show();
+        }
+
+        private void SearchBarMyList_OnSearchStarted(object? sender, FunctionEventArgs<string> e)
+        {
+            var myList = films.FindBy(ComboBoxMyList.Text, SearchBarMyList.Text);
+            if (myList != null)
+            {
+                var serialized = JsonConvert.SerializeObject(myList);
+                gridFilms.ItemsSource = JsonConvert.DeserializeObject<List<MovieSimplified>>(serialized);
+            }
         }
     }
 }
