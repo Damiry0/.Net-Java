1d623b533b8e52467608f3968c9f93ea2a873db0
Damian Ryś <55887851+Damiry0@users.noreply.github.com>
2022-05-07 15:13:15 +0200

    Created View Models

---
+++ .Net/Lab3/SpotityStats/SpotityStats/Pages/RecentlyPlayed.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SpotityStats.Pages
+{
+    public class RecentlyPlayedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ .Net/Lab3/SpotityStats/SpotityStats/Pages/TopGenres.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SpotityStats.Pages
+{
+    public class TopGenresModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
