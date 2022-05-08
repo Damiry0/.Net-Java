f42f52da024913b0ebda7b0fb8be08fe371753c3
Damian Ryś <55887851+Damiry0@users.noreply.github.com>
2022-05-09 01:43:52 +0200

    Reimplemented Authorization

--- .Net/Lab3/SpotityStats/SpotityStats/Program.cs
+++ .Net/Lab3/SpotityStats/SpotityStats/Program.cs
@@ -1,15 +1,20 @@
+using SpotityStats;
+
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-
+var startup = new Startup(builder.Configuration);
+startup.ConfigureServices(builder.Services);
 var app = builder.Build();
+startup.Configure(app);
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
---
+++ .Net/Lab3/SpotityStats/SpotityStats/Startup.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SpotifyAPI.Web;
+using static SpotifyAPI.Web.Scopes;
+
+namespace SpotityStats
+{
+    public class Startup
+    {
+        public Startup(IConfigurationRoot configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfigurationRoot Configuration { get; }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddHttpContextAccessor();
+            services.AddSingleton(SpotifyClientConfig.CreateDefault());
+            services.AddScoped<SpotifyClientBuilder>();
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("Spotify", policy =>
+                {
+                    policy.AuthenticationSchemes.Add("Spotify");
+                    policy.RequireAuthenticatedUser();
+                });
+            });
+            services
+                .AddAuthentication(options =>
+                {
+                    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                })
+                .AddCookie(options =>
+                {
+                    options.ExpireTimeSpan = TimeSpan.FromMinutes(50);
+                })
+                .AddSpotify(options =>
+                {
+                    options.ClientId = "0dfe25d5acc5413ab2376db48064fb41";
+                    options.ClientSecret =":)";
+                    options.CallbackPath = "/callback";
+                    options.SaveTokens = true;
+
+                    var scopes = new List<string> {
+                        UserReadEmail, UserReadPrivate, PlaylistReadPrivate, PlaylistReadCollaborative
+                    };
+                    options.Scope.Add(string.Join(",", scopes));
+                });
+            services.AddRazorPages()
+                .AddRazorPagesOptions(options =>
+                {
+                    options.Conventions.AuthorizeFolder("/", "Spotify");
+                });
+        }
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseAuthentication();
+            app.UseAuthorization();
+        }
+
+    }
+
+}
